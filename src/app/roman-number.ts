686e4c6bf75bb060c6222abd535a3c6ab3708647
Alberto Cangialosi <billyballo.res@gmail.com>
2023-10-05 22:31:59 +0200

    changed less strict configuration
    more specifically changed {'strict': true, 'noImplicitAny': false} inside tsconfig.json
    allowed optional properties to RomanNumber using '?:'

--- src/app/roman-number.ts
+++ src/app/roman-number.ts
@@ -1,6 +1,6 @@
 export class RomanNumber {
-  letters!: string;
-  digits!: number;
+  letters?: string; // this `?:` solve my `emit(this.dispary)` which didn't work
+  digits?: number; // this `?:` solve my `emit(this.dispary)` which didn't work
 
   constructor(letters_: string, digits_: number) {
     this.letters = letters_;
