8eebe8c9c5d7ce5ed4e8da690a88519c94d68ea1
Alberto Cangialosi <billyballo.res@gmail.com>
2023-10-05 18:03:10 +0200

    added array of RomanNumber()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { RomanNumber } from './roman-number';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Famiglia';
+
+  impero: RomanNumber[] = [
+    new RomanNumber('I', 1),
+    new RomanNumber('II', 2),
+    new RomanNumber('III', 3),
+    new RomanNumber('IV', 4),
+    new RomanNumber('V', 5),
+    new RomanNumber('VI', 6),
+    new RomanNumber('VII', 7),
+    new RomanNumber('VIII', 8),
+    new RomanNumber('IX', 9),
+    new RomanNumber('X', 10),
+  ];
 }
---
+++ src/app/roman-number.ts
@@ -0,0 +1,9 @@
+export class RomanNumber {
+  letters!: string;
+  digits!: number;
+
+  constructor(letters_: string, digits_: number) {
+    this.letters = letters_;
+    this.digits = digits_;
+  }
+}
